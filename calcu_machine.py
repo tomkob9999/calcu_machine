e60ddab7f00468fe5262df0996f35e5cd18df8a4
tokob9999 <96751911+tomkob9999@users.noreply.github.com>
2024-03-27 11:35:20 +0900

    1.1.1
    - refactored

--- calcu_machine.py
+++ calcu_machine.py
@@ -1,19 +1,38 @@
 # calcu_machine
 #
 # Description: automatically calculates total derivatives from system of equations and then solve
-# Version: 1.1.0
+# Version: 1.1.1
 # Author: Tomio Kobayashi
 # Last Update: 2024/3/27
 
 import sympy as sp
 import numpy as np
+from itertools import combinations
 
 class calcu_machine:
-    def __init__(self, equations_str, targets, variables):
+    def __init__(self, equations_str, targets, variables, is_silent=False):
         self.variables = variables
         self.targets = targets
         self.equations_str = equations_str
         self.equations = [sp.Eq(sp.sympify(eq), targets[i]) for i, eq in enumerate(self.equations_str)]
+        self.is_silent = is_silent
+        if not self.is_silent:
+            print("**************************")
+            print("Given System of Equations:", self.equations)
+
+    def chop_after_last_underscore(string):
+        # Find the index of the last "_"
+        last_underscore_index = string.rfind("_")
+
+        # If no "_" found or it's the last character, return the original string
+        if last_underscore_index == -1 or last_underscore_index == len(string) - 1:
+            return string
+
+        # Otherwise, chop the string after the last "_"
+        chopped_string = string[:last_underscore_index]
+        return chopped_string
+
+
     def solve_function(self, knowns):
         if len(knowns) != len(self.variables) - len(self.equations):
             print("Number of variables:", len(self.variables))
@@ -23,9 +42,20 @@ class calcu_machine:
         
         variables = sp.symbols(self.variables)
         eqs = [sp.Eq(sp.sympify(k), v) for k, v in knowns.items()]
-        equations = eqs + self.equations 
+        equations = self.equations + eqs
+        if not self.is_silent:
+            print("*")
+            print("Calculated System of Equations:", equations)
         unknowns = [t for t in self.variables if t not in knowns]
         solution = sp.solve(equations, self.variables)
+        
+        if isinstance(solution, dict):
+            derivs = [k for k, v in solution.items() if "_" in str(k)]
+            if len(derivs) > 0:
+                pair_combinations = list(combinations(derivs, 2))
+                for p in [pair for pair in pair_combinations if str(pair[0]).split("_")[len(str(pair[0]).split("_"))-1] == str(pair[1]).split("_")[len(str(pair[1]).split("_"))-1]]:
+                    solution[calcu_machine.chop_after_last_underscore(str(p[0])) + "_" + calcu_machine.chop_after_last_underscore(str(p[1]))] = solution[p[0]]/solution[p[1]]
+                
         return solution
     
     def derive_derivatives(self):
@@ -44,7 +74,6 @@ class calcu_machine:
                 fff = sp.sympify(str_sol)
                 partial_derivative = sp.diff(fff, inp) 
                 grads.append(partial_derivative)
-            
             new_target_vars = []
             new_vars = []
             new_equation = ""
@@ -63,21 +92,54 @@ class calcu_machine:
                 for n in new_vars:
                     self.variables.append(n)
                 variables = sp.symbols(self.variables)
-                self.equations.append(sp.Eq(sp.sympify(new_equation), sp.sympify(der_target)))
+                new_eq = sp.Eq(sp.sympify(new_equation), sp.sympify(der_target))
+                self.equations.append(new_eq)
+                if not self.is_silent:
+                    print("*")
+                    print("New Equation:", new_eq)
                 self.targets.append(der_target)
-        
+
+is_silent = True                
+# Linear
+print("===== Linear =========")
 equations = ["2 * x + 3 * y + 1 * z", "4 * x + 1 * y + 8 * z"]
 targets = [20, 30]
-calc = calcu_machine(equations, targets, ["x", "y", "z"]) 
-
+calc = calcu_machine(equations, targets, ["x", "y", "z"], is_silent=is_silent) 
 s = calc.solve_function({"z": 3})
-print(s)
+print("Solution:", s)
 calc.derive_derivatives()
-s = calc.solve_function({"z": 5})
-print(s)
-s = calc.solve_function({"x": 15})
-print(s)
-s = calc.solve_function({"y": 33})
-print(s)
+s = calc.solve_function({"z": 3})
+print("Solution with Derivatives:", s)
 
-    
+# Quadratic
+print("===== Quadratic =========")
+equations = ["2 * x + 3 * y + 1 * z", "4 * x + 1 * y**2 + 8 * z"]
+targets = [20, 30]
+calc = calcu_machine(equations, targets, ["x", "y", "z"], is_silent=is_silent) 
+s = calc.solve_function({"z": 3})
+print("Solution:", s)
+calc.derive_derivatives()
+s = calc.solve_function({"z": 3})
+print("Solution with Derivatives:", s)
+
+# Division
+print("===== Division =========")
+equations = ["1/x+1/y+1/z", "4 * x + 1 * y + 8 * z"]
+targets = [20, 30]
+calc = calcu_machine(equations, targets, ["x", "y", "z"], is_silent=is_silent) 
+s = calc.solve_function({"z": 3})
+print("Solution:", s)
+calc.derive_derivatives()
+s = calc.solve_function({"z": 3})
+print("Solution with Derivatives:", s)
+
+# Log
+print("===== Log =========")
+equations = ["2 * log(x) + 3 * y + 1 * z", "4 * x + 1 * y + 8 * z"]
+targets = [20, 30]
+calc = calcu_machine(equations, targets, ["x", "y", "z"], is_silent=is_silent) 
+s = calc.solve_function({"z": 3})
+print("Solution:", s)
+calc.derive_derivatives()
+s = calc.solve_function({"z": 3})
+print("Solution with Derivatives:", s)
